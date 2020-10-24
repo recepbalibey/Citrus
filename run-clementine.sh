2f444051580ceadcd92ea6c11a68d723eddaf90a
ruzzzzz <r.mills2@lancs.ac.uk>
2020-10-24 01:13:59 +0100

    add images

--- run-clementine.sh
+++ run-clementine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 zip -r files.zip . -x 'citrus.py' '*.git*' '*downloads*' '*__pycache__*' '*pickles*'
-spark-submit  --executor-memory 18G --jars=/dir/elasticsearch-spark-20_2.11-7.4.0.jar,/dir/spark-streaming-kafka-0-8-assembly_2.11-2.4.4.jar --py-files "files.zip" citrus.py -c
+spark-submit  --executor-memory 18G --jars=/path/to/citrus/lib/elasticsearch-spark-20_2.11-7.4.0.jar,/path/to/citrus/lib/spark-streaming-kafka-0-8-assembly_2.11-2.4.4.jar --py-files "files.zip" citrus.py -c
